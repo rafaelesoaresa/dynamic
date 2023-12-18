7caad981cb7f01b4f93c3744cc3da62d350d9e27
rafael eduardo <128144202+rafaelesoaresa@users.noreply.github.com>
2023-12-18 08:48:20 -0300

    Add files via upload

---
+++ Trasporte/src/main/java/com/spring/Trasporte/TrasporteApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Trasporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasporteApplication.class, args);
+	}
+
+}
---
+++ Trasporte/src/test/java/com/spring/Trasporte/TrasporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Trasporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
